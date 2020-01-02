ede38fb910656db0bbb7e3956c2c23d4393ec1ac
irvintarlao <irvin.tarlao@gmail.com>
2020-01-02 18:41:28 +0100

    tags

---
+++ routes/comments.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const fakeComments = require('../data/comments');
+
+const router = express.Router();
+
+
+// Get a list of comments
+router.get('/', (req, res) => {
+    res.json(fakeComments);
+  });
+
+module.exports = router;
